8c7cba94cfbfffb6dd0bc9bdb2a945d403d12029
hotege <myownroc@163.com>
2021-01-20 08:45:06 +0000

    add more compile options

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-go build -ldflags '-w -s -linkmode "external" -extldflags "-static-libgcc"'
+go build -ldflags '-w -s -linkmode "external" -extldflags "-static-libgcc"' -o sviraf
+
+if [[ $1 == "upx" ]]; then
+    upx --best sviraf
+fi
