e132b3bc633de0ef116b2bcf3e133c7c6d6ed07f
hotege <myownroc@163.com>
2021-01-20 09:20:11 +0000

    add lut3d

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 // index.js
 var selected = 0;
-function selectFile() {        
-    var input = document.getElementById("file");
-    var sourceImg = document.getElementById("source");
-    var inputText = document.getElementById("input_text");
+function selectFile(fileId, displayId, textId) {        
+    var input = document.getElementById(fileId);
+    var sourceImg = document.getElementById(displayId);
+    var inputText = document.getElementById(textId);
     input.click();
     input.onchange = event => {
         var file = event.target.files[0];
@@ -33,6 +33,25 @@ function postIrafl(url) {
     };
     xhr.send(form);
 }
+function postIraflLut3D(url) {
+    var resultImg = document.getElementById("result");
+    var form = new FormData();
+    form.append("file", document.getElementById("file").files[0]);
+    form.append("filter", document.getElementById("in_2_file").files[0]);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.responseType = "blob";
+    xhr.onload = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var reader = new FileReader();
+            reader.readAsDataURL(xhr.response);
+            reader.onload = function (e) {
+                resultImg.src = reader.result;
+            };
+        }
+    };
+    xhr.send(form);
+}
 function uploadAndFilter() {
     switch (selected) {
         case 0:
@@ -42,6 +61,7 @@ function uploadAndFilter() {
             postIrafl("/api/irafl/gaussblur?r=" + document.getElementById("in_1_radius").value);
             break;
         case 2:
+            postIraflLut3D("/api/irafl/lut3d?size=" + document.getElementById("in_2_filter_size").value);
             break;
         case 3:
             var r = document.getElementById("in_3_radius").value;
